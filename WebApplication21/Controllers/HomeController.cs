458274ea881e6920f4190dc7d67c92effe48347b
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-05-01 21:59:10 -0400

    Initial commit

--- WebApplication21/Controllers/HomeController.cs
+++ WebApplication21/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace WebApplication21.Controllers
 {
@@ -13,18 +15,79 @@ namespace WebApplication21.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult UploadImage(string someText, HttpPostedFileBase imageFile)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            string fileName = Guid.NewGuid() + Path.GetExtension(imageFile.FileName);
+            imageFile.SaveAs($"{Server.MapPath("/UploadedImages/")}{fileName}");
+            new Db(Properties.Settings.Default.ConStr)
+                .Add(fileName, someText);
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Contact()
+        public ActionResult ViewAll()
         {
-            ViewBag.Message = "Your contact page.";
+            return View(new Db(Properties.Settings.Default.ConStr).GetAll());
+        }
 
-            return View();
+        public ActionResult ViewSingle(int imageId)
+        {
+            return View(new Db(Properties.Settings.Default.ConStr).GetAll()
+                .FirstOrDefault(i => i.Id == imageId));
+        }
+    }
+
+
+    public class UploadedImage
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Db
+    {
+        private string _conStr;
+
+        public Db(string conStr)
+        {
+            _conStr = conStr;
+        }
+
+        public void Add(string fileName, string text)
+        {
+            using (var con = new SqlConnection(_conStr))
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = "INSERT INTO Images (FileName, Text) " +
+                                  "VALUES (@fileName, @text)";
+                cmd.Parameters.AddWithValue("@fileName", fileName);
+                cmd.Parameters.AddWithValue("@text", text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<UploadedImage> GetAll()
+        {
+            using (var con = new SqlConnection(_conStr))
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Images";
+                var images = new List<UploadedImage>();
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    images.Add(new UploadedImage
+                    {
+                        Id = (int)reader["Id"],
+                        FileName = (string)reader["FileName"],
+                        Text = (string)reader["Text"]
+                    });
+                }
+
+                return images;
+            }
         }
     }
 }
